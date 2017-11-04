7e1498a358b8171af5c616e1f102cc7cc12d811e
Jamie Woodmancy <jamie29w@gmail.com>
2017-11-03 19:32:46 -0700

    added basic styling to FooterComponent; started styling BodyContainer

--- client/src/main/body/Container.js
+++ client/src/main/body/Container.js
@@ -5,7 +5,38 @@ class BodyContainer extends React.Component {
         super();
     }
     render() {
-        return <div>hi from BodyContainer</div>;
+        const containerStyle = {
+            minHeight: "50vh",
+            width: "100%"
+        };
+        const pStyles = {
+            width: "80%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: "15px",
+            marginBottom: "15px",
+            fontSize: "1em"
+        };
+        return (
+            <div style={containerStyle}>
+                <div>
+                    <p style={pStyles}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Explicabo, suscipit, architecto! Vitae est,
+                        maiores adipisci cum necessitatibus sed ut provident.
+                        Iure laborum quaerat repellendus voluptatibus dolores
+                        quibusdam magni fugit dolor.
+                    </p>
+                    <p style={pStyles}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Explicabo, suscipit, architecto! Vitae est,
+                        maiores adipisci cum necessitatibus sed ut provident.
+                        Iure laborum quaerat repellendus voluptatibus dolores
+                        quibusdam magni fugit dolor.
+                    </p>
+                </div>
+            </div>
+        );
     }
 }
 
--- client/src/main/footer/Component.js
+++ client/src/main/footer/Component.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 function Footer(props) {
-    return <div>hi from Footer</div>;
+    const footerDivStyles = {
+        backgroundColor: "gray",
+        width: "100%",
+        height: "10vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around",
+        position: "relative",
+        bottom: "0px"
+    };
+
+    return (
+        <div style={footerDivStyles}>
+            <a className="linkStyles" href="">
+                blah
+            </a>
+            <a className="linkStyles" href="">
+                blah
+            </a>
+            <a className="linkStyles" href="">
+                blah
+            </a>
+            <a className="linkStyles" href="">
+                blah
+            </a>
+        </div>
+    );
 }
 
 export default Footer;

28c9089ad4fbc3c0ff9d2a128722f4b576a0bf90
Jamie Woodmancy <jamie29w@gmail.com>
2017-11-07 14:46:28 -0800

    fixed modal styliing bug where focus would change background; minor styling updates to body and footer heights

--- client/src/main/footer/Component.js
+++ client/src/main/footer/Component.js
@@ -4,27 +4,28 @@ function Footer(props) {
     const footerDivStyles = {
         backgroundColor: "rgba(18, 51, 66, 0.8)",
         width: "100%",
-        height: "10vh",
+        height: "5vh",
         display: "flex",
         alignItems: "center",
         justifyContent: "space-around",
         position: "relative",
-        bottom: "0px"
+        bottom: "0px",
+        fontStyle: "italic"
     };
 
     return (
         <div style={footerDivStyles}>
             <a className="linkStyles" href="">
-                blah
+                your
             </a>
             <a className="linkStyles" href="">
-                blah
+                custom
             </a>
             <a className="linkStyles" href="">
-                blah
+                links
             </a>
             <a className="linkStyles" href="">
-                blah
+                here
             </a>
         </div>
     );

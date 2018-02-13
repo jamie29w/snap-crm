015d46c0bd826d7f117934117e3c7b16801c0e61
Jamie Woodmancy <jamie29w@gmail.com>
2018-02-12 19:42:46 -0800

    refactored redux imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./main/App";
-import "./index.css";
-import clients from "./redux/clients";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './main/App';
+import './index.css';
+import clients from './redux/clients';
 
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { combineReducers } from "redux";
-import thunk from "redux-thunk";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
 
 let reducer = combineReducers({ clients });
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 store.subscribe(() => {
-    console.log(store.getState());
+  console.log(store.getState());
 });
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById("root")
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );

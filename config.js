ea64df272548b16c4219378eeb170fdf3920ff97
Jamie Woodmancy <jamie29w@gmail.com>
2018-09-12 23:47:48 -0700

    cleaned up config.js

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-//Uncomment secret when user auth is implemented
-
 module.exports = {
     port: process.env.PORT || 8900,
-    secret: process.env.SECRET || "blue kangaroo slippers riot fans",
     db: process.env.DB || "mongodb://localhost/clients"
 };
